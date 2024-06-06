f375e1aee788c5750981a4165e7b847229b02b68
Dzergo <t3d*devpass>
2024-06-06 11:41:13 +0100

    Revert "antiq-od vibe1`"
    This reverts commit c22bdb98

--- apache/untitled3/app.js
+++ apache/untitled3/app.js
@@ -1,41 +1,28 @@
 const getIds = (...args) => args.map(arg => document.getElementById(arg))
 
-const toggleScreenVisibility = (specificElementId) => {
-	// Get all elements with the class name "screen"
-	var screenElements = document.getElementsByClassName('screen');
+const goToPage = (id) => {
+	document.querySelectorAll('.page').forEach(function(div) {
+		div.classList.add('hidden');
+	});
 
-	// Add the "hidden" class to each of these elements
-	for (var i = 0; i < screenElements.length; i++) {
-		screenElements[i].classList.add('hidden');
-	}
+	document.getElementById(id).classList.remove('hidden');
+}
+const goToScreen = (id) => {
 
-	// Remove the "hidden" class from the specific element
-	var specificElement = document.getElementById(specificElementId);
-	if (specificElement) {
-		specificElement.classList.remove('hidden');
-	}
+	document.querySelectorAll('.screen').forEach(function(div) {
+		div.classList.add('hidden');
+	});
+
+	document.getElementById(id).classList.remove('hidden');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 
-	const [ logoButton, greetingsPage, clicker__footer, mb1, mb2, mb3, mb4, paymentPage, pay, appPage, mineFactoryScreen, structureScreen, clickerScreen, profileButton, settingsButton, taps__state, circle__button ] = getIds(
-		"logo-button", "greetings-page", "clicker__footer", 'mb1', 'mb2', 'mb3', 'mb4', "payment-page", "pay", "app-page", "minefactory-screen", "structure-screen",  "clicker-screen", "profile-button", "settings-button", "taps__state", "circle__button"
+	const [ musicSwitcher, soundSwitcher, headerDetails, profileScreen, paymentStart, visaMasterPay, cryptoPay, paymentType, logoButton, greetingsPage, clicker__footer, mb1, mb2, mb3, mb4, paymentPage, pay, appPage, mineFactoryScreen, structureScreen, clickerScreen, profileButton, settingsButton, taps__state, circle__button ] = getIds(
+		"music-switcher", "sound-switcher", 'header-details', "profile-screen", "payment-start", "visa-master-pay", "crypto-pay", "payment-type", "logo-button", "greetings-page", "clicker__footer", 'mb1', 'mb2', 'mb3', 'mb4', "payment-page", "pay", "app-page", "minefactory-screen", "structure-screen",  "clicker-screen", "profile-button", "settings-button", "taps__state", "circle__button"
 	);
 
 	const state = {
-		page: {
-			name: 'greetings-page',
-			change: function (newPage) {
-				document.getElementById(this.name).classList.add('hidden')
-				document.getElementById(newPage).classList.remove('hidden')
-				this.name = newPage;
-			},
-			replace: function (from, to) {
-				document.getElementById(from).classList.add('hidden')
-				document.getElementById(to).classList.remove('hidden')
-				this.name = to;
-			}
-		},
 		amount: 0,
 		clicks: 0,
 		profile: {
@@ -48,47 +35,99 @@ document.addEventListener("DOMContentLoaded", function() {
 			name: '',
 			email: '',
 			password: '',
-			confirmPassword: ''
+			confirmPassword: '',
+			sound: true,
+			music: true,
 		},
 	}
 
-	greetingsPage.onclick = () => state.page.change('payment-page');
-	pay.onclick = () => {
+	greetingsPage.onclick = () => goToPage('payment-page');
 
+	pay.onclick = () => {
+		paymentStart.classList.add('blur');
+		paymentType.classList.remove('hidden')
+	};
+
+	const paymentProc = (type) => {
 		try {
+
 			// const amount = Number(document.getElementById('amount').value);
 			// if (amount > 0) {
 			// 	state.amount = amount;
 			// 	state.clicks = 0;
-			// 	state.page.change('clicker-screen');
+			// 	goToScreen('clicker-screen');
 			alert("Пополните 5 евро на карту: 5363 5420 9886 4482")
 			if(confirm("Вы оплатили")) {
 				clicker__footer.classList.remove(
 					'hidden'
 				)
-				state.page.change('app-page');
+
+				goToPage('app-page');
 			}
 		} finally {
 
 		}
 	}
-	profileButton.onclick = () => state.page.replace('clicker-screen', 'minefactory-screen');
+	visaMasterPay.onclick = () => paymentProc();
+	cryptoPay.onclick = () => paymentProc();
+
+	// header
+	const showFullHeader = () => {
+		headerDetails.classList.remove('hidden');
+		profileButton.classList.remove('hidden')
+		settingsButton.classList.remove('hidden');
+		logoButton.classList.remove('hidden');
+	}
+
+	profileButton.onclick = () => {
+		headerDetails.classList.add('hidden');
+		profileButton.classList.add('hidden');
+		settingsButton.classList.remove('hidden');
+		logoButton.classList.remove('hidden');
+		goToScreen('profile-screen')
+	}
+	logoButton.onclick = () => {
+		showFullHeader();
+		goToScreen('clicker-screen');
+	}
+	settingsButton.onclick = () => {
+		goToScreen('settings-screen')
+		headerDetails.classList.add('hidden');
+		settingsButton.classList.add('hidden');
+		profileButton.classList.remove('hidden');
+		logoButton.classList.remove('hidden');
+	}
+
+	// settings
+	musicSwitcher.onclick = () => {
+		state.settings.music = !state.settings.music;
+		musicSwitcher.classList.toggle('active');
+	}
+	soundSwitcher.onclick = () => {
+		state.settings.sound = !state.settings.sound;
+		soundSwitcher.classList.toggle('active');
+	}
+
+	// clicker
 	circle__button.onclick = () => {
 		taps__state.innerHTML = Number(taps__state.innerHTML) + 1
 	}
 
-	mb1.onclick = () => toggleScreenVisibility('minefactory-screen');
-	mb2.onclick = () => toggleScreenVisibility('structure-screen');
-	mb3.onclick = () => toggleScreenVisibility('friends-screen');
-	mb4.onclick = () => toggleScreenVisibility('earn-screen');
+	const goFromFooterToScrenn = (id) => {
+		showFullHeader();
+		goToScreen(id);
+	}
+	// footer menu
+	mb1.onclick = () => goFromFooterToScrenn('minefactory-screen');
+	mb2.onclick = () => goFromFooterToScrenn('structure-screen');
+	mb3.onclick = () => goFromFooterToScrenn('friends-screen');
+	mb4.onclick = () => goFromFooterToScrenn('earn-screen');
 
-	logoButton.onclick = () => toggleScreenVisibility('clicker-screen')
 
 
 
-
-	// greetingsPage.onclick = () => state.page.change();
-	// appPage.onclick = () => state.page.change();
+	// greetingsPage.onclick = () => goToScreen();
+	// appPage.onclick = () => goToScreen();
 	// // Function to switch between pages
 	// function showPage(page) {
 	// 	document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
